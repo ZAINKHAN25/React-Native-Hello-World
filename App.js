794dc9db1c30f6ddd80b41c36c1ac04417abc9fa
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-18 10:01:44 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import './app.css';
 
 export default function App() {
+  const [dataofapi, setdataofapi] = useState([]);
+
+  useEffect(() => {
+    async function getdata() {
+      try {
+        const api = await fetch('https://fakestoreapi.com/products/');
+        const data = await api.json();
+        setdataofapi(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getdata();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="container">
+      <h1>Api Fetch</h1>
+      <div className="fatherofsingledata">
+        {dataofapi.map((x, i) => (
+          <div key={i} className="productContainer">
+            <img src={x.image} alt={x.title} className="image" />
+            <div className="productInfo">
+              <p className='title'>{x.title}</p>
+              <p className='description'>{x.description}</p>
+              <p className='price'>${x.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
